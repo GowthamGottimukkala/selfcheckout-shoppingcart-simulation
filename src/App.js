ec6f7c00fe325209f9f0790926ccde64a92f4cae
GowthamGottimukkala <gowtham.gottimukkala@gmail.com>
2020-04-25 01:21:19 +0530

    Got image file

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Barcode from './Components/Barcode'
 import Images from './Components/Images'
 import {Container, Row, Col, Button} from "react-bootstrap"
 import 'bootstrap/dist/css/bootstrap.min.css'; 
-import read from "./readfiles"  
+import filenames from "./readfiles"
 
 class App extends Component {
   constructor(props) {  
@@ -19,20 +19,8 @@ class App extends Component {
       images : [0,1,2,3,4,5],
       barcodeSelectedItem : undefined,
       imageSelected : undefined,
-      filenames: undefined
      }
   }
-
-  componentDidMount() {
-    read((result) => {
-      this.setState({
-        filenames : result
-      },function(){
-        console.log(this.state.filenames)
-      });
-    });
-  }
-
   handleBarcodeClick(id){
     this.setState({
       barcodeSelectedItem : id
@@ -62,25 +50,23 @@ class App extends Component {
   })}
 }
 
-  importAll(r) {
-    return r.keys().map(r);
-  }
-
-  handleSubmitClick(){
-    // here we use the state.barcodeselecteditem and state.imageSelected to perform validation using AWS and weights then
-    // 1. Display "Success" or "Place again" and hide Images Component
-    // 2. If "Success" Add state.bill with validated item with the help of csv
-    // 3. If "Success" Update state.barcode.display for all items according to the images available
-    // 4. If "Success" Call the function to list all the available images having the items present in the bill (and state.selectedItem is not needed
-    //    because to show fraud detection cases) ,then update state.image and unhide Images component
-    var barcodeCombinedOutput = ""
+  // here we use the state.barcodeselecteditem and state.imageSelected to perform validation using AWS and weights then
+  // 1. Display "Success" or "Place again" and hide Images Component
+  // 2. If "Success" Add state.bill with validated item with the help of csv
+  // 3. If "Success" Update state.barcode.display for all items according to the images available
+  // 4. If "Success" Call the function to list all the available images having the items present in the bill (and state.selectedItem is not needed
+  //    because to show fraud detection cases) ,then update state.image and unhide Images component
+  
+  handleSubmitClick() {
+    var barcodeCombinedOutput = []
+    var imageName = undefined
     this.state.bill.forEach(billeditem=>{
-      barcodeCombinedOutput += billeditem.id
-      barcodeCombinedOutput += "+"
+      barcodeCombinedOutput.push(billeditem.id)
     })
-    barcodeCombinedOutput += this.state.barcodeSelectedItem
+    barcodeCombinedOutput.push(this.state.barcodeSelectedItem)
     console.log(barcodeCombinedOutput)
-
+    imageName = filenames[this.state.imageSelected].name
+    console.log(imageName)
   }
 
   render() { 
@@ -91,7 +77,7 @@ class App extends Component {
             <Barcode availableitems = {this.state.barcode} barcodeClickFunction = {this.handleBarcodeClick} />
         </Col>
         <Col className="images">
-            <Images imageFiles = {this.state.images} imageClickFunction = {this.handleImageClick} submitClickFunction = {this.handleSubmitClick} />
+            <Images imageFiles = {this.state.images} imageClickFunction = {this.handleImageClick}/>
             {this.state.barcodeSelectedItem!==undefined && this.state.imageSelected!==undefined &&
               <Button onClick={this.handleSubmitClick} >Add to Cart</Button>
             }
@@ -100,7 +86,8 @@ class App extends Component {
             <Bill billeditems = {this.state.bill}/>
         </Col>
       </Row>
-    </Container>      
+    </Container>    
+      
      );
   }
 }
